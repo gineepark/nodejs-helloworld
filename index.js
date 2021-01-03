20958358cb1db89398a26c7c395512486aa45c0b
gineepark <bluejinhee@gmail.com>
2021-01-03 19:52:26 +0900

    dynamic website

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var express = require('express'); //설치한 express mudule을 불러와서 express변수에 담는다
 var app = express(); //express를 실행하여 app object를 초기화 한다
 
-app.use(express.static(__dirname + '/public'));
+app.set('view engine,''ejs'); //ejs를 사용하기 위해 express의 view engine에 set
+app.use(express.static(__dirname+'/public')) //
+
+app.get('/hello', function(req,res){
+  res.render('hello',{name:req.query.nameQuery}); //쿼리를 통해 이름을 받는 코드, 모든 query는 req.query에 저장
+});
+
+app.get('/hello/:nameParam', function(req,res){
+  res.render('hello', {name:req.params.nameParam}); //:으로 시작되는 route는 req.params에 저장됩니다.
+});
 
 var port = 3000; //사용할 포트 번호를 port변수에 담는다
 
